81f2066d0c8bf19df86a2e22e172924e8755c9ea
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-04-18 01:02:48 +0300

    format

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            double a, p, S;
+            Console.WriteLine("Input perimeter:");
+            double perimeter = double.Parse(Console.ReadLine());
+            a = perimeter / 3; //исходя из условия треугольник равносторонний
+            p = perimeter / 2;
+
+            S = Math.Sqrt(p * Math.Pow((p - a), 3));
+
+            // Console.WriteLine($"Сторона  Площадь");
+            // Console.WriteLine($"  {a}      {S:F2}");
+
+            Console.WriteLine("{0,-10}{1,10}", "Сторона", "Площадь");
+            Console.WriteLine("{0,-10:F2}{1,10:F2}", a, S);
+
         }
     }
 }
